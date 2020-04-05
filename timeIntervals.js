4c4e30ad81f01ee82b26b5ec7273867d68851d75
spenc259 <hello@paulspence.me.uk>
2020-04-05 12:36:54 +0100

    removed unnecessary methods and tidied up the code

--- timeIntervals.js
+++ timeIntervals.js
@@ -36,8 +36,10 @@ const solve = (workers) => {
     
     let q3 = Question3(workers);
     for (let i = 0; i < q3.length; i++) {
-        console.log(q3[i]);
+        console.info(q3[i]);
     }
+
+    console.info("\n\nsolver complete!\n\n");
 }
 
 /* Questions */
@@ -76,66 +78,31 @@ const Question2 = (workers) => {
 }
 
 
-
 const Question3 = (workers) => {
     console.info("\n\nQuestion 3\nWhat are the intervals of date and times (in UTC) where there are at least 2 workers free?");
 
-    let starts = []
-    let ends = []
     let overlaps = []
     let times = []
-    // compare each workers endtime to every other workers start time
-    // if the endtime is greater than the other start time and less than the other end time we have an overlap
-    workers.forEach( (w, i) => {
-        // get a list of start and end times for the current worker
-        intervalStarts = getStartTimes( getIntervals(w) );
-        starts.push( intervalStarts );
-
-        intervalEnds = getEndTimes( getIntervals(w) );
-        ends.push( intervalEnds )
-
-    } )
-
-    // map though each end array
-    ends.map( (cw,ind) => {
-        // for each end item
-        for (let e = 0; e < cw.length; e++) {
-            // map through each start array
-            starts.map((v, i) => {
-                // loop the start dates in the sub arrays
-                for (let s = 0; s < v.length; s++) {
-                    // is the current end time greater than the current start time 
-                    // AND is the current start time less than the end time for another worker
-                    // Astart < Bend && Aend > Bstart 
-                    // AND to make the overlap stricter we say is the current worker start time less than the other workers end time
-                    // if (cw[e] > v[s] && cw[e] < ends[i][s] && starts[ind][e] > v[s] && starts[ind][e] < ends[i][s]) {
-                    // if () {
-                    //     overlaps.push(v[s] + '/' + cw[e]);
-                    // }
-                }
-            })
-        }
-
-    })
 
     workers.forEach((w, index) => {
         times.push(getIntervals(w));
     })
 
     // is my start less than their end AND is my end greater than their start
-    // (StartA <= EndB) and (EndA >= StartB)
     /// A = 1-----3
     /// B =    2-----4
 
-    // let times = [['1/3','5/7'], ['2/4', '6/8']];
     // create an array of times start/end
-    // foreach time, split and loop through the times array, split and compare
+    // foreach time, loop through the sub array [v], split into start and end times
+    // loop through again to get the other times to compare against
+    // push result of comparasion into the overlaps array
     times.forEach((v,wi) => {
 
         for(let i = 0; i < v.length; i++) {
             
-            let starttime = new Date(v[i].split('/')[0]); // 1 // new date to convert the times to UTC
-            let endTime = new Date(v[i].split('/')[1]); // 3
+            // new date to convert the times to UTC
+            let starttime = new Date(v[i].split('/')[0]); 
+            let endTime = new Date(v[i].split('/')[1]); 
 
             times.forEach((v,wj) => {
 
@@ -145,7 +112,7 @@ const Question3 = (workers) => {
                     let otherEndTime = new Date(othertimes[1]) // 4
                     
                     if (starttime < otherEndTime && endTime > otherStartTime && wi !== wj) {
-                        // console.log(new Date(Math.max(starttime, otherStartTime)).toISOString() + '/' + new Date(Math.min(endTime,otherEndTime)).toISOString());
+                        // we need to use new date again here to output the ISO string format
                         overlaps.push(new Date(Math.max(starttime, otherStartTime)).toISOString() + '/' + new Date(Math.min(endTime,otherEndTime)).toISOString())
                     }
                 }
@@ -153,32 +120,12 @@ const Question3 = (workers) => {
         }
     }) 
 
-    
-
     // ensure we have unique values
+    // we may have duplicates due to workers being off at different times
     return [...new Set(overlaps)]
 }
 
 /* Helpers */
-
-const getStartTimes = (intervals) => {
-    return intervals.map( (int, index) => {
-        // split into start and end
-        let dates = int.split('/');
-        // convert both into UTC
-        return new Date(dates[0]);
-    })
-}
-
-const getEndTimes = (intervals) => {
-    return intervals.map( (int, index) => {
-        // split into start and end
-        let dates = int.split('/');
-        // convert both into UTC
-        return new Date(dates[1]);
-    })
-}
-
 const getIntervals = (sw) => {
     return sw.split(',');
 }
